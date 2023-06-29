7536904a4d8e9577452d2878979e39a7bf5e9b6f
Mark Cabalona <markcabalona58@gmail.com>
2023-06-29 22:10:41 +0800

    #1: add comment feature directory, resize video stream size, add viewers count

---
+++ lib/features/Comment/presentation/widgets/comment_section_widget.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/widgets.dart';
+
+class CommentSectionWidget extends StatelessWidget {
+  const CommentSectionWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [],
+    );
+  }
+}
