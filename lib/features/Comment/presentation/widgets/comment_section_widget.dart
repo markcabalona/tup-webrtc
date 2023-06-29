ae0bf5705211c3e71a0b67a4255719ce43c25c18
Mark Cabalona <markcabalona58@gmail.com>
2023-06-29 23:40:35 +0800

    #fix: redesign comment widget

--- lib/features/Comment/presentation/widgets/comment_section_widget.dart
+++ lib/features/Comment/presentation/widgets/comment_section_widget.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tuplive/core/constants/grid.dart';
 import 'package:tuplive/core/dependencies/dependencies.dart';
+import 'package:tuplive/core/presentation/widgets/profile_pic_widget.dart';
+import 'package:tuplive/core/presentation/widgets/spacers.dart';
 import 'package:tuplive/core/utils/datetime_extension.dart';
 import 'package:tuplive/features/Comment/presentation/cubit/comment_cubit.dart';
 import 'package:tuplive/features/Comment/presentation/widgets/comment_field.dart';
@@ -20,26 +23,56 @@ class CommentSectionWidget extends StatelessWidget {
         ..fetchAndSubscribeToComments(
           roomID: roomID,
         ),
-      child: BlocBuilder<CommentCubit, CommentState>(
-        builder: (context, state) {
-          return Column(
-            children: [
-              ...state.comments.map(
-                (e) => Text(
-                  "${e.dateCreated.toFormattedTime()} ${e.author.name}: ${e.comment}",
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: AppGrid.x2Small),
+        child: BlocBuilder<CommentCubit, CommentState>(
+          builder: (context, state) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ...state.comments.map(
+                  (e) => Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          ProfilePicWidget(
+                            radius: 25,
+                            imageUrl: e.author.profileImgUrl,
+                          ),
+                          HorizontalSpacers.x2Small,
+                          Text(
+                            e.author.name,
+                          ),
+                          const Spacer(),
+                          Text(
+                            "${e.dateCreated.toFormattedTime(format: 'hh:mm:ss')} ${e.dateCreated.hour > 11 ? 'PM' : 'AM'}",
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(left: 25 + AppGrid.x2Small),
+                        child: Text(e.comment),
+                      ),
+                      VerticalSpacers.small,
+                    ],
+                  ),
                 ),
-              ),
-              CommentField(
-                onSubmit: (comment) {
-                  BlocProvider.of<CommentCubit>(context).createComment(
-                    roomID: roomID,
-                    comment: comment,
-                  );
-                },
-              )
-            ],
-          );
-        },
+                CommentField(
+                  onSubmit: (comment) {
+                    BlocProvider.of<CommentCubit>(context).createComment(
+                      roomID: roomID,
+                      comment: comment,
+                    );
+                  },
+                )
+              ],
+            );
+          },
+        ),
       ),
     );
   }
