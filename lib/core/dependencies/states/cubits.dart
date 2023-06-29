f54c0ab747a26df3c6075d4fe16c05a659e1dc6e
Mark Cabalona <markcabalona58@gmail.com>
2023-06-29 23:25:01 +0800

    #feature: add comment cubit to dependencies

--- lib/core/dependencies/states/cubits.dart
+++ lib/core/dependencies/states/cubits.dart
@@ -3,6 +3,9 @@ import 'package:tuplive/core/dependencies/dependencies.dart'
     show serviceLocator;
 import 'package:tuplive/core/theme/cubits/theme_cubit/app_theme_cubit.dart';
 import 'package:tuplive/features/Auth/presentation/cubit/auth_cubit.dart';
+import 'package:tuplive/features/Comment/data/datasources/comment_datasource.dart';
+import 'package:tuplive/features/Comment/data/repositories/comment_repository_impl.dart';
+import 'package:tuplive/features/Comment/presentation/cubit/comment_cubit.dart';
 import 'package:tuplive/features/LiveStream/presentation/cubit/active_streams_cubit.dart';
 
 void initializeCubits() {
@@ -18,5 +21,13 @@ void initializeCubits() {
 
   serviceLocator.registerLazySingleton<AuthCubit>(
     () => AuthCubit(),
+  );
+
+  serviceLocator.registerFactory<CommentCubit>(
+    () => CommentCubit(
+      CommentRepositoryImpl(
+        datasource: CommentDatasource(),
+      ),
+    ),
   );
 }
