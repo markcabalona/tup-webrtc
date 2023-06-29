020eeb8cb495930f022f6da3b174602dd7bce343
Mark Cabalona <markcabalona58@gmail.com>
2023-06-29 23:02:34 +0800

    #feature: add firestore integration on `CommentDatasource.fetchAndSubToComments`

--- lib/features/Comment/data/datasources/comment_datasource.dart
+++ lib/features/Comment/data/datasources/comment_datasource.dart
@@ -1,12 +1,33 @@
+import 'package:tuplive/core/errors/exceptions.dart';
 import 'package:tuplive/core/mixins/api_handler_mixin.dart';
 import 'package:tuplive/features/Auth/domain/entities/user.dart';
+import 'package:tuplive/features/Comment/data/models/comment_model.dart';
 import 'package:tuplive/features/Comment/domain/entities/comment.dart';
 
 class CommentDatasource with FirestoreHandlerMixin {
   Future<Stream<List<Comment>>> fetchAndSubToComments({
     required String roomID,
   }) async {
-    throw UnimplementedError();
+    return firestoreHandler(
+      request: () async {
+        final commentCollection =
+            firestore.collection('rooms').doc(roomID).collection('comments');
+        final commentSnapshots = commentCollection.snapshots();
+
+        return commentSnapshots.map(
+          (snap) => snap.docs
+              .map(
+                (doc) => CommentModel.fromJson(
+                  doc.data(),
+                ),
+              )
+              .toList(),
+        );
+      },
+      onFailure: (error) {
+        throw AppException(message: error.message);
+      },
+    );
   }
 
   Future<void> unsubscribeToComments({
